2412c8c74e0f2e8cb666cea08569659c42fefe95
Victor Grechi <victorgrechi@hotmail.com>
2020-06-04 21:53:43 -0300

    Criando as entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public ICollection<Pedido> Pedidos { get; set; }
+
+
+    }
+}
